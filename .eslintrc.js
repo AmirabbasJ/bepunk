ec1a7a7ffa68dc21c4f24d9afbfb7f2d31fa971a
AmirabbasJ <amirabbasgamer2@gmail.com>
2024-01-18 12:24:30 +0330

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+const { init } = require('@fullstacksjs/eslint-config/init');
+
+module.exports = init({
+  root: true,
+  modules: {
+    auto: true,
+    typescript: {
+      parserProject: ['./tsconfig.eslint.json'],
+      resolverProject: ['./tsconfig.json'],
+    },
+  },
+  rules: {
+    // NOTE: https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/no-unused-prop-types.md#known-issueslimitations
+    'react/no-unused-prop-types': 'off',
+    // NOTE: Lot of false positive in props
+    'react/jsx-no-leaked-render': 'off',
+  },
+});
