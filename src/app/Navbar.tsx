267f6e08fd79a7d21855574fef6c0b72a46322d1
AmirabbasJ <amirabbasgamer2@gmail.com>
2024-01-21 00:15:35 +0330

    add cart indicator

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,21 +1,34 @@
-import { ActionIcon, Button, Group } from '@mantine/core';
+import { ActionIcon, Button, Group, Indicator } from '@mantine/core';
 import { IconHome, IconShoppingCart } from '@tabler/icons-react';
 
+import { useCartCache } from '@/cache';
+
 export const Navbar = () => {
+  const { cart: ids } = useCartCache();
+  const cartItemsCount = ids.length;
   return (
     <Group h="100%" px="md">
       <Button component="a" href="/" leftSection={<IconHome />} variant="light">
         Home
       </Button>
-      <ActionIcon
-        component="a"
-        href="/cart"
-        color="green"
-        variant="light"
-        size="lg"
+      <Indicator
+        position="top-end"
+        color="teal"
+        offset={2}
+        disabled={cartItemsCount === 0}
+        size={15}
+        label={cartItemsCount}
       >
-        <IconShoppingCart />
-      </ActionIcon>
+        <ActionIcon
+          component="a"
+          href="/cart"
+          color="green"
+          variant="light"
+          size="lg"
+        >
+          <IconShoppingCart />
+        </ActionIcon>
+      </Indicator>
     </Group>
   );
 };
